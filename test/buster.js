18e6837d95d1ae82c2a065e4e888a27292e5be26
Christian Johansen <christian@gitorious.org>
2011-08-15 23:36:19 +0200

    Add section for browser tests in buster.js

--- test/buster.js
+++ test/buster.js
@@ -4,3 +4,10 @@ config["Node tests"] = {
     environment: "node",
     tests: ["test/node/**/*.js"]
 };
+
+config["Browser tests"] = {
+    environment: "browser",
+    src: ["lib/buster-test-cli/browser/wiring.js"],
+    tests: ["test/browser/test-helper.js",
+            "test/browser/**/*.js"]
+};
