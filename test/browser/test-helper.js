867db01848a8749c7e62006a4fb42ba531e09449
Christian Johansen <christian@gitorious.org>
2011-10-09 00:53:35 +0200

    Rewrite browser-wiring
    - Give the thing an actual API
    - Make messaging object injectable
    - Fix tests and test config

--- test/browser/test-helper.js
+++ test/browser/test-helper.js
@@ -1,16 +1,15 @@
-buster.multicastClient = buster.eventEmitter.create();
+function createRemoteEmitter() {
+    var emitter = buster.eventEmitter.create();
 
-buster.multicastClient.emit = function (event, data) {
-    return buster.eventEmitter.emit.call(this, event, {
-        data: data,
-        topic: event
-    });
-};
+    emitter.emit = function (event, data) {
+        return buster.eventEmitter.emit.call(this, event, {
+            data: data,
+            topic: event
+        });
+    };
 
-buster.resetMulticastClient = function () {
-    delete buster.multicastClient.listeners;
-    delete buster.multicastClient.contexts;
-};
+    return emitter;
+}
 
 buster.testRunner.onCreate(function (runner) {
     buster.wiredRunner = runner;
--- test/browser/wiring-test.js
+++ test/browser/wiring-test.js
@@ -1,17 +1,14 @@
-(function () {
+(function (B) {
     if (typeof require == "function" && typeof module == "object") {
         return;
     }
 
-    buster.assert.fail = function (message) {
-        fail(message);
-    };
-
     buster.util.testCase("BrowserWiringTest", {
         setUp: function () {
-            //buster.resetMulticastClient();
+            this.emitter = createRemoteEmitter();
+            B.configureTestClient(this.emitter);
+
             this.bnt = buster.nextTick;
-            buster.assert.throwOnFailure = true;
 
             buster.nextTick = function (callback) {
                 callback();
@@ -20,15 +17,11 @@
 
         tearDown: function () {
             buster.nextTick = this.bnt;
-
-            if (buster.assert.bind.restore) {
-                buster.assert.bind.restore();
-            }
         },
 
         "should set up console and buster.log shortcut": function () {
-            buster.assert.isObject(buster.console);
-            buster.assert.isFunction(buster.log);
+            assertObject(buster.console);
+            assertFunction(buster.log);
         },
 
         "should format log messages nicely": function () {
@@ -36,7 +29,7 @@
             buster.console.on("log", function (msg) { message = msg.message });
             buster.console.log("Hello world", { id: 42 });
 
-            buster.assert.equals(message, "Hello world { id: 42 }");
+            assertEquals("Hello world { id: 42 }", message);
         },
 
         "should log messages through buster.log": function () {
@@ -49,47 +42,47 @@
 
         "should send log messages over the wire": function () {
             var listener = sinon.spy();
-            buster.multicastClient.on("log", listener);
+            this.emitter.on("log", listener);
 
             buster.console.warn("Hello world", { id: 42 });
+            assertTrue(listener.calledOnce);
 
-            buster.assert(listener.calledOnce);
-            buster.assert.equals(listener.args[0][0], {
-                topic: "log",
-                data: {
-                    message: "Hello world { id: 42 }",
-                    level: "warn"
-                }
-            });
+            var msg = listener.args[0][0];
+            assertEquals("log", msg.topic);
+
+            assertEquals({
+                message: "Hello world { id: 42 }",
+                level: "warn"
+            }, msg.data);
         },
 
         "should start running tests when emitting tests:run": function () {
             var listener = sinon.spy();
-            buster.multicastClient.on("suite:start", listener);
+            this.emitter.on("suite:start", listener);
 
             buster.testCase("Test", { "should do it": function () {} });
-            buster.multicastClient.emit("tests:run");
+            this.emitter.emit("tests:run");
 
-            buster.assert(listener.calledOnce);
+            assertTrue(listener.calledOnce);
         },
 
         "should run specs when emitting tests:run": function () {
             var listener = sinon.spy();
-            buster.multicastClient.on("context:start", listener);
+            this.emitter.on("context:start", listener);
 
             buster.spec.describe("Spec", function () {
                 buster.spec.should("do it", function () {});
             });
 
-            buster.multicastClient.emit("tests:run");
+            this.emitter.emit("tests:run");
 
-            buster.assert(listener.calledOnce);
-            buster.assert.equals(listener.args[0][0].data.name, "Spec");
+            assertTrue(listener.calledOnce);
+            assertEquals(listener.args[0][0].data.name, "Spec");
         },
 
         "should run parsable context when emitting tests:run": function () {
             var listener = sinon.spy();
-            buster.multicastClient.on("context:start", listener);
+            this.emitter.on("context:start", listener);
 
             buster.addTestContext({
                 parse: function () {
@@ -97,35 +90,25 @@
                 }
             });
 
-            buster.multicastClient.emit("tests:run");
+            this.emitter.emit("tests:run");
 
-            buster.assert(listener.calledOnce);
-            buster.assert.equals(listener.args[0][0].data.name, "Parsed");
+            assertTrue(listener.calledOnce);
+            assertEquals(listener.args[0][0].data.name, "Parsed");
         },
 
         "should create test runner with options": function () {
-            buster.multicastClient.emit("tests:run", {
+            this.emitter.emit("tests:run", {
                 timeout: 25, failOnNoAssertions: false
             });
 
-            buster.assert.equals(buster.wiredRunner.timeout, 25);
-            buster.assert.isFalse(buster.wiredRunner.failOnNoAssertions);
+            assertEquals(buster.wiredRunner.timeout, 25);
+            assertFalse(buster.wiredRunner.failOnNoAssertions);
         },
 
         "should configure assertions to not throw": function () {
-            buster.multicastClient.emit("tests:run");
+            this.emitter.emit("tests:run");
 
-            buster.assert.isFalse(buster.assert.throwOnFailure);
-        },
-
-        "should subscribe test runner to assertion failure": function () {
-            sinon.spy(buster.assert, "bind");
-            buster.multicastClient.emit("tests:run");
-
-            buster.assert(buster.assert.bind.calledOnce);
-            buster.assert(buster.assert.bind.calledWith(buster.wiredRunner, {
-                "failure": "assertionFailure"
-            }));
+            assertFalse(buster.assertions.throwOnFailure);
         },
 
         "should count assertions": function () {
@@ -146,9 +129,9 @@
                 },
             });
 
-            buster.multicastClient.emit("tests:run");
+            this.emitter.emit("tests:run");
 
-            buster.assert.equals(counts, [1, 2]);
+            assertEquals(counts, [1, 2]);
         }
     });
-}());
+}(buster));
