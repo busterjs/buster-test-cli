e603efb0df54c35a8dccb7bdd2d26bf15d6455cc
Christian Johansen <christian@gitorious.org>
2012-01-29 16:46:21 +0100

    Update config helper to work with new buster-resources/buster-configuration

--- test/node/unit/config-test.js
+++ test/node/unit/config-test.js
@@ -18,86 +18,79 @@ buster.testCase("Test client configuration", {
         process.chdir(__dirname);
         this.config = testConfig.create();
         this.config.addGroup("Client tests", {});
-        this.config.addGroup("Server tests", { environment: "node", load: ["something.js"] });
+        this.config.addGroup("Server tests", {
+            environment: "node",
+            load: ["something.js"]
+        });
     },
 
     tearDown: helper.clientTearDown,
 
-    "should preload session configuration with library": function (done) {
-        extendConfigGroup(this.config, "browser", function (configGroup) {
-            var res = configGroup.resources;
+    "preloads session configuration with library": function (done) {
+        extendConfigGroup(this.config, "browser", done(function (rs) {
+            assert.equals(rs.length, 35);
+            assert.isObject(rs.get("/buster/buster-core.js"));
+            assert.isObject(rs.get("/buster/buster-event-emitter.js"));
+            assert.isObject(rs.get("/buster/buster-evented-logger.js"));
+            assert.isObject(rs.get("/buster/buster-assertions.js"));
+            assert.isObject(rs.get("/buster/buster-assertions/expect.js"));
+            assert.isObject(rs.get("/buster/buster-format.js"));
+            assert.isObject(rs.get("/buster/buster-promise.js"));
+            assert.isObject(rs.get("/buster/sinon.js"));
+            assert.isObject(rs.get("/buster/sinon/spy.js"));
+            assert.isObject(rs.get("/buster/sinon/stub.js"));
+            assert.isObject(rs.get("/buster/sinon/mock.js"));
+            assert.isObject(rs.get("/buster/sinon/collection.js"));
+            assert.isObject(rs.get("/buster/sinon/sandbox.js"));
+            assert.isObject(rs.get("/buster/sinon/test.js"));
+            assert.isObject(rs.get("/buster/sinon/test_case.js"));
+            assert.isObject(rs.get("/buster/sinon/assert.js"));
+            assert.isObject(rs.get("/buster/sinon/util/fake_xml_http_request.js"));
+            assert.isObject(rs.get("/buster/sinon/util/fake_timers.js"));
+            assert.isObject(rs.get("/buster/sinon/util/fake_server.js"));
+            assert.isObject(rs.get("/buster/sinon/util/fake_server_with_clock.js"));
+            assert.isObject(rs.get("/buster/buster-test/spec.js"));
+            assert.isObject(rs.get("/buster/buster-test/test-case.js"));
+            assert.isObject(rs.get("/buster/buster-test/test-context.js"));
+            assert.isObject(rs.get("/buster/buster-test/test-runner.js"));
+            assert.isObject(rs.get("/buster/buster-test/reporters/json-proxy.js"));
+            assert.isObject(rs.get("/buster/sinon-buster.js"));
+            assert.isObject(rs.get("/buster/bundle-" + version + ".js"));
+            assert.isObject(rs.get("/buster/sinon/util/timers_ie.js"));
+            assert.isObject(rs.get("/buster/sinon/util/xhr_ie.js"));
+            assert.isObject(rs.get("/buster/buster/buster-wiring.js"));
+            assert.isObject(rs.get("/buster/wiring.js"));
 
-            assert.isObject(res["/buster/buster-core.js"]);
-            assert.isObject(res["/buster/buster-event-emitter.js"]);
-            assert.isObject(res["/buster/buster-evented-logger.js"]);
-            assert.isObject(res["/buster/buster-assertions.js"]);
-            assert.isObject(res["/buster/buster-assertions/expect.js"]);
-            assert.isObject(res["/buster/buster-format.js"]);
-            assert.isObject(res["/buster/buster-promise.js"]);
-            assert.isObject(res["/buster/sinon.js"]);
-            assert.isObject(res["/buster/sinon/spy.js"]);
-            assert.isObject(res["/buster/sinon/stub.js"]);
-            assert.isObject(res["/buster/sinon/mock.js"]);
-            assert.isObject(res["/buster/sinon/collection.js"]);
-            assert.isObject(res["/buster/sinon/sandbox.js"]);
-            assert.isObject(res["/buster/sinon/test.js"]);
-            assert.isObject(res["/buster/sinon/test_case.js"]);
-            assert.isObject(res["/buster/sinon/assert.js"]);
-            assert.isObject(res["/buster/sinon/util/fake_xml_http_request.js"]);
-            assert.isObject(res["/buster/sinon/util/fake_timers.js"]);
-            assert.isObject(res["/buster/sinon/util/fake_server.js"]);
-            assert.isObject(res["/buster/sinon/util/fake_server_with_clock.js"]);
-            assert.isObject(res["/buster/buster-test/spec.js"]);
-            assert.isObject(res["/buster/buster-test/test-case.js"]);
-            assert.isObject(res["/buster/buster-test/test-context.js"]);
-            assert.isObject(res["/buster/buster-test/test-runner.js"]);
-            assert.isObject(res["/buster/buster-test/reporters/json-proxy.js"]);
-            assert.isObject(res["/buster/sinon-buster.js"]);
-            assert.isObject(res["/buster/bundle-" + version + ".js"]);
-            assert.isObject(res["/buster/sinon/util/timers_ie.js"]);
-            assert.isObject(res["/buster/sinon/util/xhr_ie.js"]);
-            assert.isObject(res["/buster/buster/buster-wiring.js"]);
-            assert.isObject(res["/buster/wiring.js"]);
-
-            assert.equals(configGroup.load, [
+            assert.equals(rs.loadPath.paths(), [
                 "/buster/bundle-" + version + ".js",
                 "/buster/compat-" + version + ".js",
                 "/buster/wiring.js",
                 "/buster/ready.js"]);
-
-            done();
-        });
+        }));
     },
 
-    "should load ready script last": function (done) {
+    "loads ready script last": function (done) {
         var config = testConfig.create();
         config.addGroup("Client tests", {
-            rootPath: process.cwd,
+            rootPath: process.cwd(),
             tests: ["config-test.js"]
         });
 
-        extendConfigGroup(config, "browser", function (configGroup) {
-            var res = configGroup.resources;
-
-            assert.equals(configGroup.load, [
+        extendConfigGroup(config, "browser", done(function (rs) {
+            assert.equals(rs.loadPath.paths(), [
                 "/buster/bundle-" + version + ".js",
                 "/buster/compat-" + version + ".js",
                 "/buster/wiring.js",
                 "/config-test.js",
                 "/buster/ready.js"]);
-
-            done();
-        });
+        }));
     }
 });
 
 function extendConfigGroup(config, env, callback) {
     var group = config.filterEnv(env).groups[0];
-    group.resolve().then(function (resourceSet) {
-        configHelper.extendConfigurationGroupWithWiring(group);
-
-        resourceSet.getReadOnly(function (err, conf) {
-            callback(conf);
-        });
-    }, function (err) { buster.log(err); });
+    configHelper.bundleFramework(group).resolve().then(
+        callback,
+        function (err) { buster.log(err); }
+    );
 }
