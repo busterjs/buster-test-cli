500f4a274117bcf0cf6a3b385c741444d11edfd4
Christian Johansen <christian@cjohansen.no>
2012-05-16 13:38:03 +0200

    Rearrange layout
      - Test runners don't need the -runner name suffix any more
      - Browser runner related modules (progress reporter, remote runner)
        live in runners/browser
      - Ruby-like IO objects replaced with writable streams

--- test/runners/browser/progress-reporter-test.js
+++ test/runners/browser/progress-reporter-test.js
@@ -1,17 +1,16 @@
-var helper = require("../../test-helper");
 var buster = require("buster");
-buster.remoteRunner = helper.require("test-runner/remote-runner");
-buster.progressReporter = helper.require("test-runner/progress-reporter");
-var assert = buster.assert;
+var remoteRunner = require("../../../lib/runners/browser/remote-runner");
+var progressReporter = require("../../../lib/runners/browser/progress-reporter");
 var S = require("buster-terminal");
+var cliHelper = require("buster-cli/lib/test-helper");
 
 buster.testCase("Progress reporter", {
     setUp: function () {
         this.clock = this.useFakeTimers();
-        this.multicaster = buster.eventEmitter.create();
+        this.messageClient = buster.eventEmitter.create();
 
         this.emit = function (event, data, client) {
-            return this.multicaster.emit(event, {
+            return this.messageClient.emit(event, {
                 topic: event,
                 data: data,
                 clientId: client
@@ -34,82 +33,73 @@ buster.testCase("Progress reporter", {
             }
         }];
 
-        this.runner = buster.remoteRunner.create(this.multicaster);
+        this.runner = remoteRunner.create(this.messageClient);
         this.runner.createClient(1, this.clients[0]);
         this.runner.createClient(2, this.clients[1]);
+        this.stdout = cliHelper.writableStream("stdout");
 
-        this.io = {
-            out: "",
-            toString: function () { return this.out; },
-            print: function (str) { this.out += str; },
-            puts: function (str) { this.out += str + "\n"; }
-        };
-
-        this.reporter = buster.progressReporter.create({
-            io: this.io
+        this.reporter = progressReporter.create({
+            outputStream: this.stdout
         }).listen(this.runner);
     },
 
     "should not print anything without clients": function () {
         this.emit("test:success", {});
-
-        assert.equals(S.stripSeq(this.io.toString()), "");
+        assert.equals(S.stripSeq(this.stdout.toString()), "");
     },
 
-    "should print client when adding": function () {
+    "prints client when adding": function () {
         this.reporter.addClient(1, this.clients[0]);
         this.reporter.addClient(2, this.clients[1]);
 
-        assert.match(this.io.toString(), "Chrome 9.0.597.107 Linux:");
-        assert.match(this.io.toString(), "Firefox 4.0 Linux:");
+        assert.stdout("Chrome 9.0.597.107 Linux:");
+        assert.stdout("Firefox 4.0 Linux:");
     },
 
-    "should print dot for test success": function () {
+    "print dot for test success": function () {
         this.reporter.addClient(1, this.clients[0]);
         this.reporter.addClient(2, this.clients[1]);
-        this.io.out = "";
         this.emit("test:success", {}, 1);
 
-        assert.match(this.io.toString(), ".");
+        assert.stdout(".");
     },
 
-    "should print E for test error": function () {
+    "prints E for test error": function () {
         this.reporter.addClient(1, this.clients[0]);
         this.reporter.addClient(2, this.clients[1]);
-        this.io.out = "";
         this.emit("test:error", {}, 1);
 
-        assert.match(this.io.toString(), "E");
+        assert.stdout("E");
     },
 
-    "should print F for test failure": function () {
+    "prints F for test failure": function () {
         this.reporter.addClient(1, this.clients[0]);
         this.reporter.addClient(2, this.clients[1]);
-        this.io.out = "";
         this.emit("test:failure", {}, 1);
 
-        assert.match(this.io.toString(), "F");
+        assert.stdout("F");
     },
 
-    "should print T for test timeout": function () {
+    "prints T for test timeout": function () {
         this.reporter.addClient(1, this.clients[0]);
         this.reporter.addClient(2, this.clients[1]);
-        this.io.out = "";
         this.emit("test:timeout", {}, 1);
 
-        assert.match(this.io.toString(), "T");
+        assert.stdout("T");
     },
 
-    "should save uncaught exceptions until browser connects": function () {
+    "saves uncaught exceptions until browser connects": function () {
         this.reporter.uncaughtException(1, "Oops");
-        refute.match(this.io.toString(), "Oops");
+        refute.stdout("Oops");
+
         this.reporter.addClient(1, this.clients[0]);
-        assert.match(this.io.toString(), "Oops");
+        assert.stdout("Oops");
     },
 
     "immediately prints uncaught exception for known client": function () {
         this.reporter.addClient(1, this.clients[0]);
         this.reporter.uncaughtException(1, "Oops");
-        assert.match(this.io.toString(), "Oops");
+
+        assert.stdout("Oops");
     }
 });
