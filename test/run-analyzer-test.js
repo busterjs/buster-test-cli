e39c7837086c46cc643e2d42d7c6e68165b314ad
Christian Johansen <christian@cjohansen.no>
2012-05-21 14:16:35 +0200

    Analyzer helper is now runAnalyzer
      - An object that starts and analyzes a test run

---
+++ test/run-analyzer-test.js
@@ -0,0 +1,109 @@
+var buster = require("buster");
+var runAnalyzer = require("../lib/run-analyzer");
+var cliHelper = require("buster-cli/lib/test-helper");
+var stdioLogger = require("buster-stdio-logger");
+
+buster.testCase("Analyzer helper", {
+    setUp: function () {
+        this.stdout = cliHelper.writableStream("stdout");
+        this.stderr = cliHelper.writableStream("stderr");
+        this.logger = stdioLogger(this.stdout, this.stderr);
+    },
+
+    "sets fail level on analyzer": function () {
+        var analyzer = runAnalyzer.create(this.logger, { failOn: "warning" });
+        analyzer.warning("Oh noes");
+        assert(analyzer.status().failed);
+    },
+
+    "file reporter": {
+        "prints to stderr": function () {
+            var analyzer = runAnalyzer.create(this.logger, {});
+            analyzer.warning("Oh noes");
+            assert.stderr("Oh noes");
+            assert.stdout(/^$/);
+        }
+    },
+
+    "run analysis": {
+        setUp: function () {
+            this.analyzer = runAnalyzer.create(this.logger, {});
+            this.runner = { run: this.spy(), abort: this.spy() };
+            this.config = { runExtensionHook: this.spy() };
+        },
+
+        "prevents caching on warning": function () {
+            this.analyzer.run(this.runner, this.config);
+            this.analyzer.emit("warning", {});
+
+            assert.isFalse(this.runner.cacheable);
+        },
+
+        "prevents caching on error": function () {
+            this.analyzer.run(this.runner, this.config);
+            this.analyzer.emit("error", {});
+
+            assert.isFalse(this.runner.cacheable);
+        },
+
+        "prevents caching on fatal": function () {
+            this.analyzer.run(this.runner, this.config);
+            this.analyzer.emit("fatal", {});
+
+            assert.isFalse(this.runner.cacheable);
+        },
+
+        "triggers analyze extension hook": function () {
+            this.analyzer.run(this.runner, this.config);
+
+            var hook = this.config.runExtensionHook;
+            assert.calledOnce(hook);
+            assert.calledOnceWith(hook, "analyze", this.analyzer);
+        },
+
+        "starts run": function () {
+            this.analyzer.run(this.runner, this.config);
+
+            assert.calledOnce(this.runner.run);
+            assert.calledWith(this.runner.run, this.config, {});
+        },
+
+        "aborts run if analyzer fails": function () {
+            this.analyzer.run(this.runner, this.config);
+            this.analyzer.emit("fail", { errors: 42 });
+
+            assert.calledOnce(this.runner.abort);
+            assert.match(this.runner.abort.args[0][0], {
+                stats: { errors: 42 },
+                type: "AnalyzerError",
+                message: "Pre-condition failed"
+            });
+        },
+
+        "calls callback when analyzer fails run": function () {
+            var callback = this.spy();
+            this.analyzer.run(this.runner, this.config, callback);
+            this.analyzer.emit("fail", { errors: 42 });
+
+            assert.calledOnce(callback);
+        },
+
+        "only calls callback once": function () {
+            var callback = this.spy();
+            this.analyzer.run(this.runner, this.config, callback);
+            this.analyzer.emit("fail", { errors: 42 });
+            this.runner.run.yield(null);
+
+            assert.calledOnce(callback);
+        },
+
+        "only calls callback once when analyzer fails after run": function () {
+            var callback = this.spy();
+            this.analyzer.run(this.runner, this.config, callback);
+            this.runner.run.yield(null);
+            this.analyzer.emit("fail", { errors: 42 });
+
+            assert.calledOnce(callback);
+        }
+    }
+});
